107831ac98ca50d7018fc6211e5772d8a9e86838
Geverson Souza <geversonjosedeosouza@gmail.com>
2021-09-09 00:05:32 -0300

    Adição do link de redirecionamento para cadastro.

--- SisContatos/pages/frmMaster.Master.designer.cs
+++ SisContatos/pages/frmMaster.Master.designer.cs
@@ -24,21 +24,12 @@ namespace SisContatos.pages
         protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
 
         /// <summary>
-        /// Controle form1.
+        /// Controle contentPlaceHolderFrmMaster.
         /// </summary>
         /// <remarks>
         /// Campo gerado automaticamente.
         /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
         /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
-
-        /// <summary>
-        /// Controle ContentPlaceHolder1.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder contentPlaceHolderFrmMaster;
     }
 }
