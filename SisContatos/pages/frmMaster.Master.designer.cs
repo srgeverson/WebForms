a4909a3860f84d1e2ff5977d61eda181a28f7b64
Geverson Souza <geversonjosedeosouza@gmail.com>
2021-09-09 15:05:59 -0300

    remocao da tag form

--- SisContatos/pages/frmMaster.Master.designer.cs
+++ SisContatos/pages/frmMaster.Master.designer.cs
@@ -24,15 +24,6 @@ namespace SisContatos.pages
         protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
 
         /// <summary>
-        /// Controle form1.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
-
-        /// <summary>
         /// Controle btnCadastrarContato.
         /// </summary>
         /// <remarks>
