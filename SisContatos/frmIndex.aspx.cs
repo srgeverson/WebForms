ff7b9c614652ef7173f342af33a5758432b9a342
Geverson Souza <geversonjosedeosouza@gmail.com>
2021-09-08 22:33:19 -0300

    Renomeando arquivo para inicialização.

--- SisContatos/frmIndex.aspx.cs
+++ SisContatos/frmIndex.aspx.cs
@@ -7,44 +7,11 @@ using System.Web.UI.WebControls;
 
 namespace SisContatos
 {
-    public partial class frmIndex : System.Web.UI.Page
-    {
-        private static int incrementa = 0;
+	public partial class frmIndex1 : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
 
-        protected void Page_Load(object sender, EventArgs e)
-        {
-            mostrarLista();
-        }
-        
-        protected void btnAdd_Click(object sender, EventArgs e)
-        {
-            var contato = new Contato();
-            incrementa = incrementa + 1;
-            contato.Id = incrementa;
-            contato.Nome = txtNome.Text;
-            contato.Salvar();
-            mostrarLista();
-        }
-
-        private void mostrarLista()
-        {
-            pnlCadastro.Visible = false;
-            pnlLista.Visible = true;
-
-            grvContatos.DataSource = Contato.Contatos;
-            grvContatos.DataBind();
-        }
-
-        private void mostrarCadastro()
-        {
-            pnlCadastro.Visible = true;
-            pnlLista.Visible = false;
-
-        }
-
-        protected void btnCadastro_Click(object sender, EventArgs e)
-        {
-            mostrarCadastro();
-        }
-    }
+		}
+	}
 }
--- SisContatos/frmIndex.aspx.designer.cs
+++ SisContatos/frmIndex.aspx.designer.cs
@@ -11,70 +11,7 @@ namespace SisContatos
 {
 
 
-    public partial class frmIndex
+    public partial class frmIndex1
     {
-
-        /// <summary>
-        /// Controle form1.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
-
-        /// <summary>
-        /// Controle pnlCadastro.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Panel pnlCadastro;
-
-        /// <summary>
-        /// Controle txtNome.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox txtNome;
-
-        /// <summary>
-        /// Controle btnAdd.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnAdd;
-
-        /// <summary>
-        /// Controle pnlLista.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Panel pnlLista;
-
-        /// <summary>
-        /// Controle btnCadastro.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnCadastro;
-
-        /// <summary>
-        /// Controle grvContatos.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.GridView grvContatos;
     }
 }
