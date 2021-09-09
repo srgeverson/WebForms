75c230909dcb4b243721a1a294d20c728ef6b987
Geverson Souza <geversonjosedeosouza@gmail.com>
2021-09-09 00:04:18 -0300

    Formulário para cadastro de contatos.

---
+++ SisContatos/pages/frmCadastrarContato.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/pages/frmMaster.Master" AutoEventWireup="true" CodeBehind="frmCadastrarContato.aspx.cs" Inherits="SisContatos.pages.WebForm1" %>
+
+<asp:Content ID="contentFrmCadastrarContatoHead" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="contentFrmCadastrarContato" ContentPlaceHolderID="contentPlaceHolderFrmMaster" runat="server">
+    <form id="formSalvarContato" runat="server">
+        <div class="row">
+            <div class="col-md-6">
+                <asp:Label ID="lblNome" for="txtNome" class="form-label" runat="server">Nome</asp:Label>
+                <asp:TextBox type="text" class="form-control" ID="txtNome" runat="server" />
+            </div>
+            <div class="col-md-6">
+                <asp:Label ID="lblSobrenome" for="txtSobrenome" class="form-label" runat="server">Sobre Nome</asp:Label>
+                <asp:TextBox type="text" class="form-control" ID="txtSobrenome" runat="server" />
+            </div>
+        </div>
+        <div class="col-12">
+            <asp:Label ID="lblEmail" for="txtEmail" class="form-label" runat="server">E-mail</asp:Label>
+            <asp:TextBox type="email" class="form-control" ID="txtEmail" placeholder="Seu email" runat="server" />
+        </div>
+        <div class="col-12">
+            <asp:Label ID="lblTelefone" for="txtTelefone" class="form-label" runat="server">Telefone</asp:Label>
+            <asp:TextBox type="tel" class="form-control" ID="txtTelefone" placeholder="Seu telefone" runat="server" />
+        </div>
+        <div class="col-12 mt-2">
+            <asp:Button ID="btnSalvar" type="submit" Text="Salvar" class="btn btn-primary" runat="server" />
+        </div>
+    </form>
+</asp:Content>
---
+++ SisContatos/pages/frmCadastrarContato.aspx.designer.cs
@@ -0,0 +1,107 @@
+﻿//------------------------------------------------------------------------------
+// <gerado automaticamente>
+//     Este código foi gerado por uma ferramenta.
+//
+//     As alterações ao arquivo poderão causar comportamento incorreto e serão perdidas se
+//     o código for recriado
+// </gerado automaticamente>
+//------------------------------------------------------------------------------
+
+namespace SisContatos.pages
+{
+
+
+    public partial class WebForm1
+    {
+
+        /// <summary>
+        /// Controle formSalvarContato.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm formSalvarContato;
+
+        /// <summary>
+        /// Controle lblNome.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblNome;
+
+        /// <summary>
+        /// Controle txtNome.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtNome;
+
+        /// <summary>
+        /// Controle lblSobrenome.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblSobrenome;
+
+        /// <summary>
+        /// Controle txtSobrenome.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtSobrenome;
+
+        /// <summary>
+        /// Controle lblEmail.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblEmail;
+
+        /// <summary>
+        /// Controle txtEmail.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtEmail;
+
+        /// <summary>
+        /// Controle lblTelefone.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblTelefone;
+
+        /// <summary>
+        /// Controle txtTelefone.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtTelefone;
+
+        /// <summary>
+        /// Controle btnSalvar.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnSalvar;
+    }
+}
