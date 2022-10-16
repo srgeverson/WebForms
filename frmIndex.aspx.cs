faa2cd3fcdd03ef53cac61b62c08c48353f6efc0
Geverson <geversonjosedesouza@gmail.com>
2022-10-16 18:01:29 -0300

    Incluindo dependencia do nuget.org

--- frmIndex.aspx.cs
+++ frmIndex.aspx.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace SisContatos
 {
-    public partial class WebForm1 : System.Web.UI.Page
+    public partial class frmIndex : Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
